7d814ccb207d59c8dc027ddcfeaeccc6fb482e17
Nurbol Y <yessnurbol@gmail.com>
2021-10-05 19:00:32 +0600

    AppBarSettings

--- lib/modules/main_page/presentation/view/main_view.dart
+++ lib/modules/main_page/presentation/view/main_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:surf_test/constants/colors/colors_styles.dart';
+import 'package:surf_test/constants/text/text_styles.dart';
 
 class MainView extends StatefulWidget {
   MainView({Key? key}) : super(key: key);
@@ -8,9 +11,63 @@ class MainView extends StatefulWidget {
 }
 
 class _MainViewState extends State<MainView> {
+  TextEditingController searchController = new TextEditingController();
+  int symbolCount = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: ColorStyles.app_black_222325,
+      appBar: AppBar(
+        backgroundColor: ColorStyles.app_black_222325,
+        title: Container(
+          width: double.infinity,
+          height: 40.h,
+          child: TextField(
+            controller: searchController,
+            onChanged: (String val) {
+              setState(() {
+                symbolCount = val.length;
+              });
+            },
+            decoration: InputDecoration(
+              prefixIcon: IconButton(
+                icon: Icon(Icons.search, color: ColorStyles.app_blue_2443FF),
+                onPressed: () {
+                  print('hello');
+                },
+              ),
+              suffixIcon: symbolCount > 0
+                  ? IconButton(
+                      icon: Icon(
+                        Icons.clear,
+                        color: ColorStyles.app_blue_2443FF,
+                      ),
+                      onPressed: () {
+                        searchController.clear();
+                        setState(() {
+                          symbolCount = 0;
+                        });
+                      },
+                    )
+                  : null,
+              filled: true,
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 10.h, horizontal: 10.w),
+              fillColor: Colors.white,
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide.none,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide.none,
+              ),
+              hintText: 'Поиск по название фильма',
+              hintStyle: TextStyles.grey_14_w500,
+            ),
+          ),
+        ),
+      ),
       body: SafeArea(
         child: Container(
           child: Text('hello world'),
